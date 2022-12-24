4de6699a0e2cd5ce637b4f973d1883aa35f93f33
azamatyudaev <azamatyudaev@gmail.com>
2022-12-24 16:06:53 +0500

    Reducers

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function buyCake() {
     type: BUY_CAKE
   }
 }
+
+const initialState = {
+  numberOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE: return {
+      ...state,
+      numberOfCakes: state.numberOfCakes - 1
+    }
+
+    default: return state
+  }
+}
