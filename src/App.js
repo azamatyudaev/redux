338eaadc967d777c92cc287f03e4a5aa005e5afc
azamatyudaev <azamatyudaev@gmail.com>
2022-12-25 01:11:07 +0500

    Redux with React starter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CakeContainer from './components/cakeContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
