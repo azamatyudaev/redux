c364d16ba611818ca44ea9928835e8ecebbc809b
azamatyudaev <azamatyudaev@gmail.com>
2022-12-24 23:52:35 +0500

    Async actions

---
+++ asyncActions.js
@@ -0,0 +1,62 @@
+import redux from 'redux'
+
+const createStore = redux.createStore
+
+const initialState = {
+  users: [],
+  loading: false,
+  error: ''
+}
+
+// Action types
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
+
+// Action creators
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  }
+}
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case FETCH_USERS_REQUEST: return {
+      ...state,
+      loading: true
+    }
+
+    case FETCH_USERS_SUCCESS: return {
+      ...state,
+      loading: false,
+      users: action.payload,
+      error: ''
+    }
+
+    case FETCH_USERS_FAILURE: return {
+      ...state,
+      loading: false,
+      users: [],
+      error: action.payload
+    }
+
+    default: return state
+  }
+}
+
+const store = createStore(reducer)
