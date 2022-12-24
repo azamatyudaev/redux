86d64b21356cf31c18653232232f7d7481b12dbc
azamatyudaev <azamatyudaev@gmail.com>
2022-12-24 20:01:15 +0500

    Multiple reducers

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 import redux from 'redux'
 const createStore = redux.createStore
 
-// Action type
+// Action types
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
-// Action creator function that returns an object
+// Action creators function that returns an object
 function buyCake() {
   return {
     type: BUY_CAKE
   }
 }
 
-const initialState = {
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM
+  }
+}
+
+const initialCakeState = {
   numberOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numberOfIceCreams: 20
+}
+
+const CakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE: return {
       ...state,
       numberOfCakes: state.numberOfCakes - 1
+    }
+
+    default: return state
+  }
+}
+
+const IceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM: return {
+      ...state,
+      numberOfIceCreams: state.numberOfIceCreams - 1
     }
 
     default: return state
@@ -35,5 +57,7 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 
 unsubscribe()
