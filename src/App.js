9a5bbbfa13eea0fdea7c3dbea705157e027c308f
azamatyudaev <azamatyudaev@gmail.com>
2022-12-25 23:14:53 +0500

    Redux React with useSelector hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CakeContainer from './components/cakeContainer';
 import './App.css';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 function App() {
   return (
     <div className="App">
+      <HooksCakeContainer />
       <CakeContainer />
     </div>
   );
---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { buyCake } from '../redux'
+
+const HooksCakeContainer = () => {
+  const dispatch = useDispatch()
+  const numberOfCakes = useSelector((state) => state.numberOfCakes)
+
+  return (
+    <div>
+      <h2>Number of Cakes - {numberOfCakes}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
+    </div>
+  )
+}
+
+export default HooksCakeContainer
